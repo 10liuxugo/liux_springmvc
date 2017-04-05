b5c9c869e3bc7c09ac4f9ca6ae6704d082af5f7c
10liuxugo <10liuxugo@gmail.com>
2017-04-05 17:10:12 +0800

    add  org.apache.commons.logging

--- src/com/liux/sys/web/UserController.java
+++ src/com/liux/sys/web/UserController.java
@@ -1,7 +1,10 @@
 package com.liux.sys.web;
 
 import java.util.List;
+import java.util.Properties;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.poi.hssf.record.formula.functions.Var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,8 +15,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.liux.sys.entity.UserVo;
 import com.liux.sys.service.UserService;
 
+import jdk.nashorn.internal.runtime.linker.JavaAdapterFactory;
+
 @Controller
 public class UserController {
+
+	private static Log runLog = LogFactory.getLog("runLogger");// 运行日志
+	private static Log interfaceLog = LogFactory.getLog("interfaceLogger");// 接口日志
 
 	@Autowired
 	private UserService userService;
@@ -21,6 +29,8 @@ public class UserController {
 	@RequestMapping("/index")
 	public String login(ModelMap modelMap) {
 
+		interfaceLog.info("====>>请求");
+		runLog.info("请求02");
 		UserVo u = new UserVo();
 		u = userService.login(u);
 
